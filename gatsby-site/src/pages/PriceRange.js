8b80861cbfac09fa4541e87f9a6d031ac7ae427b
gachez <briangach98@gmail.com>
2020-08-28 20:23:42 +0300

    added the home page and price

--- gatsby-site/src/pages/PriceRange.js
+++ gatsby-site/src/pages/PriceRange.js
@@ -1,9 +1,134 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
+import {Card, Button, Image} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Background from '../img/tv.jpg';
+import SmartPhone from '../img/phone.jpg';
+import Fridge from '../img/fridge.jpg'; 
+import Sofa from '../img/sofa.jpg';
+import Jumia from '../img/jumia.png';
+import Jiji from '../img/jiji.jpg';
+import Kilimall from '../img/kilimall.png';
+import Masoko from '../img/masoko.jpg';
+import Logo from '../img/logo.png';
+import Money from '../img/money.png';
+import Conservative from '../img/conmoney.png';
+import Affordable from '../img/budget.png';
+import Generous from '../img/generous.png';
 
-export default function PriceRage() {
-  return (<div>
-      PriceRange
-      </div>
-  
-  )}
+const cards = [
+  {
+    color: 'green',
+    title: 'Affordable',
+    range: '0 - 20,000',
+    img: Affordable
+  },
+  {
+    color: 'red',
+    title: 'Conservative',
+    range: '20,000 - 80,000',
+    img: Conservative
+  },
+  {
+    color: 'blue',
+    title: 'Generous',
+    range: '80,000 - 250,000',
+    img: Money
+  },
+  {
+    color: 'yellow',
+    title: 'Extravagant',
+    range: '250,000+',
+    img: Generous
+  }
+];
+
+const sources = [
+  {
+    companyTitle: 'Jumia',
+    companyLogo: Jumia
+  },
+  {
+    companyTitle: 'Masoko',
+    companyLogo: Masoko
+  },
+  {
+    companyTitle: 'Jiji',
+    companyLogo: Jiji
+  },
+  {
+    companyTitle: 'Kilimall',
+    companyLogo: Kilimall
+  }
+];
+
+
+export default function PriceRange() {
+
+return (
+<div style={{
+    backgroundColor: '#1e2129',
+    position: 'fixed',
+    height: '100%',
+    width: '100%'
+}}>
+<Image src={Logo} style={{
+  position: 'absolute',
+  width: '128px',
+  height: '64px',
+  left: '1vw',
+  top: '2%',
+  margin: '10px'
+}} />    
+
+<h4 style={{
+    marginTop: '7.5%',
+    marginLeft: '7vw',
+    color: '#8e95a5'
+}}>What's your budget?</h4>
+   
+<div style={{
+      display: 'flex',
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      width: '90vw',
+      marginLeft: '5vw'
+    }}>
+      {
+        cards.map( card => {
+          return (
+            <Card key={card.title} style={{ width: '18rem', margin: '2rem', borderRadius: '8px'}}>
+              <Card.Img onMouseOver = {() => console.log('hov')} src={card.img} style={{ width: '100%', height: '60%', objectFit: 'contain', padding: '10px'}} />
+              <Card.Body style={{  borderRadius: '8px' }}>
+              <Card.Title style={{ color: '#000'}}>{card.title}</Card.Title>
+              <Card.Text style={{ fontWeight: 'bold'}} >KSH {card.range}</Card.Text>
+                <Link to="/PriceRange">
+                  <Button variant="primary">Continue</Button>
+                  </Link>
+              </Card.Body>
+            </Card>
+          )
+        })
+      }
+    </div>
+    <div style={{
+      width: '100vw',
+      justifyContent: 'center',
+      alignContent: 'center',
+      alignItems: 'center',
+      marginTop: '1rem',
+      display: 'flex',
+      flexDirection:'row',
+      flexWrap: 'wrap'
+    }}>
+      {
+        sources.map(source => {
+          return(
+            <Image style={{ width: '80px', height: '80px', borderRadius: '50%', margin: '1rem' }} src={source.companyLogo} />
+          )
+        })
+      }
+    </div>
+    
+</div>) 
+}
--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -3,48 +3,87 @@ import { Link } from "gatsby";
 import {Card, Button, Image} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Background from '../img/tv.jpg';
+import SmartPhone from '../img/phone.jpg';
+import Fridge from '../img/fridge.jpg'; 
+import Sofa from '../img/sofa.jpg';
+import Logo from '../img/logo.png';
+import Jumia from '../img/jumia.png';
+import Jiji from '../img/jiji.jpg';
+import Kilimall from '../img/kilimall.png';
+import Masoko from '../img/masoko.jpg';
+import '../styles/style.css';
 
 
 const cards = [
   {
     color: 'green',
-    title: 'Televisions'
+    title: 'Television',
+    img: Background
   },
   {
     color: 'red',
-    title: 'Smart Phones'
+    title: 'Smart Phone',
+    img: SmartPhone
   },
   {
     color: 'blue',
-    title: 'Fridges'
+    title: 'Fridge',
+    img: Fridge
   },
   {
     color: 'yellow',
-    title: 'Laptops'
+    title: 'Sofa',
+    img: Sofa
+  }
+];
+
+const sources = [
+  {
+    companyTitle: 'Jumia',
+    companyLogo: Jumia
+  },
+  {
+    companyTitle: 'Masoko',
+    companyLogo: Masoko
+  },
+  {
+    companyTitle: 'Jiji',
+    companyLogo: Jiji
+  },
+  {
+    companyTitle: 'Kilimall',
+    companyLogo: Kilimall
   }
 ]
 
 export default function Home() {
-  return (
-  <div style={{
+
+return (
+<div style={{
     backgroundColor: '#1e2129',
     position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0
-  }}>
-<h1 style={{
-    marginTop: '2.5%',
+    height: '100%',
+    width: '100%'
+}}>
+<Image src={Logo} style={{
+  position: 'absolute',
+  width: '128px',
+  height: '64px',
+  left: '1vw',
+  top: '2%',
+  margin: '10px'
+}} />  
+<h2 style={{
+    marginTop: '10%',
     marginLeft: '7vw',
     color: '#00acc1'
-}}>Welcome to Bei Poa</h1><br />
+}}>Welcome to Bei Poa</h2><br />
 
-<h3 style={{
+<h4 style={{
     marginTop: '.5%',
     marginLeft: '7vw',
     color: '#8e95a5'
-}}>What do you want to buy?🛍️</h3>
+}}>What do you want to buy?🛍️</h4>
    
 <div style={{
       display: 'flex',
@@ -57,12 +96,32 @@ export default function Home() {
         cards.map( card => {
           return (
             <Card key={card.title} style={{ width: '18rem', margin: '2rem', borderRadius: '8px'}}>
-              <Card.Img src={Background} style={{ width: '100%', height: 'auto', objectFit: 'cover'}} />
+              <Link to="/PriceRange">
+                <Card.Img src={card.img} style={{ height: '250px'}} />
+                </Link> 
               <Card.Body style={{  borderRadius: '8px' }}>
-              <Card.Title>{card.title}</Card.Title>
-                <Button variant="primary">Continue</Button>
+              <Card.Title style={{ color: '#000'}}>{card.title}</Card.Title>
+            
               </Card.Body>
             </Card>
+          )
+        })
+      }
+    </div>
+    <div style={{
+      width: '100vw',
+      justifyContent: 'center',
+      alignContent: 'center',
+      alignItems: 'center',
+      marginTop: '1rem',
+      display: 'flex',
+      flexDirection:'row',
+      flexWrap: 'wrap'
+    }}>
+      {
+        sources.map(source => {
+          return(
+            <Image style={{ width: '80px', height: '80px', borderRadius: '50%', margin: '1rem' }} src={source.companyLogo} />
           )
         })
       }
