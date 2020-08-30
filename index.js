3bf1529d82eb1e2210370d99a46ebf8ec82e34c4
gachez <briangach98@gmail.com>
2020-08-30 21:20:36 +0300

    to be continued...

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const {  scrapeProductsKilimall, scrapeProductsJumia } = require('./lib/functions/scrapeProductDetails');
+const {  scrapeProductsKilimall, scrapeProductsJumia, scrapeProductsMasoko } = require('./lib/functions/scrapeProductDetails');
 
 // scrapeProductsJumia('https://www.jumia.co.ke/mlp-smart-tvs/');
-scrapeProductsKilimall('https://www.kilimall.co.ke/new/commoditysearch?c=1255&aside=smart%20tv&gc_id=1255');
+
+//scrapeProductsKilimall('https://www.kilimall.co.ke/new/commoditysearch?c=1255&aside=smart%20tv&gc_id=1255');
+
+scrapeProductsMasoko('https://www.masoko.com/all-categories/phones-tablets/mobile-phones?SID=ejaq9bh33i779qbsdji8hoffc2')
