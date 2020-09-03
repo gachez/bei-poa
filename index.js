33834ab4a85408dafde355fc1aaec3c4eef3df28
gachez <briangach98@gmail.com>
2020-09-03 12:45:05 +0300

    export the price comparison engine

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const {  scrapeProductsKilimall, scrapeProductsJumia, scrapeProductsMasoko } = require('./lib/functions/scrapeProductDetails');
+const {priceComparisonEngine} = require('./lib/functions/priceComparisonEngine');
 
+let range = [10000, 12000];
  //scrapeProductsJumia('https://www.jumia.co.ke/mlp-smart-tvs/');
 
-scrapeProductsKilimall('https://www.kilimall.co.ke/new/commoditysearch?c=1074&aside=null&gc_id=1074');
+//scrapeProductsKilimall('https://www.kilimall.co.ke/new/commoditysearch?c=1074&aside=null&gc_id=1074');
 
 //scrapeProductsMasoko('https://www.masoko.com/all-categories/phones-tablets/mobile-phones')
+
+priceComparisonEngine(range, scrapeProductsKilimall('https://www.kilimall.co.ke/new/commoditysearch?c=1074&aside=null&gc_id=1074'));
