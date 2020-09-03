467271e336457b0be4953538c8f2e93681218f67
gachez <briangach98@gmail.com>
2020-09-03 12:30:43 +0300

    fix: function to scrape

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const {  scrapeProductsKilimall, scrapeProductsJumia, scrapeProductsMasoko } = require('./lib/functions/scrapeProductDetails');
 
-// scrapeProductsJumia('https://www.jumia.co.ke/mlp-smart-tvs/');
+ //scrapeProductsJumia('https://www.jumia.co.ke/mlp-smart-tvs/');
 
-//scrapeProductsKilimall('https://www.kilimall.co.ke/new/commoditysearch?c=1255&aside=smart%20tv&gc_id=1255');
+scrapeProductsKilimall('https://www.kilimall.co.ke/new/commoditysearch?c=1074&aside=null&gc_id=1074');
 
-scrapeProductsMasoko('https://www.masoko.com/all-categories/phones-tablets/mobile-phones?SID=ejaq9bh33i779qbsdji8hoffc2')
+//scrapeProductsMasoko('https://www.masoko.com/all-categories/phones-tablets/mobile-phones')
